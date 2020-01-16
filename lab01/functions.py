b08c2340e62999ff84fc341a5a3da0b41afc0ed2
Young Kim <31612885+chillinYoung@users.noreply.github.com>
2020-01-15 22:42:32 -0800

    fix base_conversion() to stop calculation when qutient is zero

--- lab01/functions.py
+++ lab01/functions.py
@@ -22,25 +22,35 @@ def base_conversion():
     base_num = int(input("Enter base number between 2 and 9: "))
     max_num = ((base_num ** 4) - 1)
 
+    first_num = ""
+    second_num = ""
+    third_num = ""
+    fourth_num = ""
+
     if decimal_num <= max_num:
         # calculate remainder and store it to variable
         # keep qutient to calculate later one
         first_num = str(decimal_num % base_num)
         qutient = decimal_num // base_num
 
-        second_num = str(qutient % base_num)
-        qutient = qutient // base_num
+        # if quitient is zero, calculation stops
+        if qutient != 0:
+            second_num = str(qutient % base_num)
+            qutient = qutient // base_num
 
-        third_num = str(qutient % base_num)
-        qutient = qutient // base_num
+            if qutient != 0:
+                third_num = str(qutient % base_num)
+                qutient = qutient // base_num
 
-        fourth_num = str(qutient % base_num)
+                if qutient != 0:
+                    fourth_num = str(qutient % base_num)
 
-        print(fourth_num + third_num + second_num + first_num)
+        # print final converted number by adding nums reversed
+        print(fourth_num + third_num + second_num + first_num + "(" + str(base_num) + ")")
 
     else:
         # input number is more than 4 digits in base number
-        print("The number is too big to convert!")
+        print("# ERROR: the number is too big to convert!")
 
 
 
@@ -53,16 +63,20 @@ if __name__ == '__main__':
     input_first = input("Enter your first name: ")
     input_last = input("Enter your last name: ")
     format_name(input_first, input_last)
+    print()
 
     # to test tripler()
     input_tripler = input("Enter any character or word to repeat: ")
     tripler(input_tripler)
+    print()
 
     # to test this_year()
     this_year()
+    print()
 
     # to test base_conversion()
     base_conversion()
+    print()
 
 
 
