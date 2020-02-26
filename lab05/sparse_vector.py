30cb0972e631d474b36dd1f770ee661b5fdb3cb2
Young Kim <31612885+chillinYoung@users.noreply.github.com>
2020-02-26 00:58:01 -0800

    added doctests

--- lab05/sparse_vector.py
+++ lab05/sparse_vector.py
@@ -3,6 +3,8 @@ COMP 1510 202010 Lab 05
 Young Kim (A01087377)
 """
 
+import doctest
+
 
 def sparse_add(sparse_vector1: dict, sparse_vector2: dict) -> dict:
     """Add two vectors.
@@ -16,10 +18,14 @@ def sparse_add(sparse_vector1: dict, sparse_vector2: dict) -> dict:
     :postcondition: calculate addition of two sparse vectors correctly
     :return: a dictionary
 
-    >>> test_vec1 = {2: 5, 5: 1, 9: 1, 'length': 10}
+    >>> test_vec1 = {5: 1, 2: 5, 9: 1, 'length': 10}
     >>> test_vec2 = {'length': 10, 3: 2, 7: 5, 9: 3}
     >>> sparse_add(test_vec1, test_vec2)
     {'length': 10, 2: 5, 3: 2, 5: 1, 7: 5, 9: 4}
+    >>> test_vec1 = {'length': 10, 2: -5, 5: 1, 9: -1}
+    >>> test_vec2 = {'length': 10, 3: 2, 7: -5, 9: -3}
+    >>> sparse_add(test_vec1, test_vec2)
+    {'length': 10, 2: -5, 3: 2, 5: 1, 7: -5, 9: -4}
     >>> test_vec1 = {'length': 2, 1: 1}
     >>> test_vec2 = {'length': 2, 1: -1}
     >>> sparse_add(test_vec1, test_vec2)
@@ -50,6 +56,23 @@ def sparse_dot_product(sparse_vector1: dict, sparse_vector2: dict):
                     vector's length in key and value pair
     :postcondition: calculate dot product of two sparse vectors correctly
     :return: a number (integer or float)
+
+    >>> test_vec1 = {'length': 8, 1: -1, 3: 2, 6: 1}
+    >>> test_vec2 = {'length': 8, 3: -2, 4: 1, 6: -4}
+    >>> sparse_dot_product(test_vec1, test_vec2)
+    -8
+    >>> test_vec1 = {'length': 3, 1: 2}
+    >>> test_vec2 = {'length': 3, 1: 1}
+    >>> sparse_dot_product(test_vec1, test_vec2)
+    2
+    >>> test_vec1 = {'length': 5, 1: 1, 3: 2}
+    >>> test_vec2 = {'length': 5, 2: 2, 4: 1}
+    >>> sparse_dot_product(test_vec1, test_vec2)
+    0
+    >>> test_vec1 = {'length': 5}
+    >>> test_vec2 = {'length': 5}
+    >>> sparse_dot_product(test_vec1, test_vec2)
+    0
     """
     if sparse_vector1['length'] != sparse_vector2['length']:
         return None
@@ -69,9 +92,18 @@ def convert_vector_dict_to_list(vector_in_dict: dict) -> list:
     :postcondition: correctly convert the given dictionary vector to a list
                     vector with tagged vector length
     :return: a list
+
+    >>> convert_vector_dict_to_list({'length': 5, 3: 1})
+    [0, 0, 0, 1, 0]
+    >>> convert_vector_dict_to_list({2: 3, 1: 1, 0: 1, 4: 3, 'length': 6})
+    [1, 1, 3, 0, 3, 0]
+    >>> convert_vector_dict_to_list({'length': 7})
+    [0, 0, 0, 0, 0, 0, 0]
+    >>> convert_vector_dict_to_list({'length': 6, 2: 1, 1: -4, 0: 1, 5: -3 })
+    [1, -4, 1, 0, 0, -3]
     """
-    return [vector_in_dict[key] if key in vector_in_dict else 0
-            for key in range(vector_in_dict['length'])]
+    return [vector_in_dict[index] if index in vector_in_dict else 0
+            for index in range(vector_in_dict['length'])]
 
 
 def convert_vector_list_to_dict(vector_in_list: list) -> dict:
@@ -82,6 +114,15 @@ def convert_vector_list_to_dict(vector_in_list: list) -> dict:
     :postcondition: correctly convert the given list vector to a dictionary
                     vector including tag for the vector length
     :return: a dictionary
+
+    >>> convert_vector_list_to_dict([0, 1, 3, 0, 0, 0])
+    {'length': 6, 1: 1, 2: 3}
+    >>> convert_vector_list_to_dict([0, 0, 0, 0, 0])
+    {'length': 5}
+    >>> convert_vector_list_to_dict([-3, 0, 7, -2, 0, 0, 1])
+    {'length': 7, 0: -3, 2: 7, 3: -2, 6: 1}
+    >>> convert_vector_list_to_dict([1, 0, 0, 0])
+    {'length': 4, 0: 1}
     """
     dict_converted = {'length': len(vector_in_list)}
     counter = 0
@@ -95,12 +136,9 @@ def convert_vector_list_to_dict(vector_in_list: list) -> dict:
 
 def main():
     """
-    Drive the Program.
+    Drive the doctest in this module.
     """
-    test_vec1 = {2: 5, 5: 1, 9: 1, 'length': 10}
-    test_vec2 = {'length': 10, 3: 2, 7: 5, 9: 3}
-    print(sparse_dot_product(test_vec1, test_vec2))
-    print(sparse_add(test_vec1, test_vec2))
+    doctest.testmod()
 
 
 if __name__ == "__main__":
