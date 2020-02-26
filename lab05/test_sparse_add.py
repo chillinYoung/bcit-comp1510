7ad947244f9c99f6f80cdb3f17c07e714e05a3fd
Young Kim <31612885+chillinYoung@users.noreply.github.com>
2020-02-26 00:58:34 -0800

    added a little more of test functions

--- lab05/test_sparse_add.py
+++ lab05/test_sparse_add.py
@@ -23,16 +23,37 @@ class TestSparseVector(TestCase):
         actual = sparse_add(test_vec1, test_vec2)
         self.assertEqual(actual, expected)
 
-    def test_sparse_add_different_position_of_length(self):
+    def test_sparse_add_length_on_the_first_position(self):
         expected = {'length': 5, 1: 2, 4: 7}
-        test_vec1 = {1: 2, 'length': 5}
+        test_vec1 = {'length': 5, 1: 2}
         test_vec2 = {'length': 5, 4: 7}
         actual = sparse_add(test_vec1, test_vec2)
         self.assertEqual(actual, expected)
 
-    def test_sparse_add_different_length_of_vectors(self):
+    def test_sparse_add_different_position_of_length(self):
+        expected = {'length': 5, 1: 2, 3: 1, 4: 7}
+        test_vec1 = {1: 2, 'length': 5}
+        test_vec2 = {3: 1, 'length': 5, 4: 7}
+        actual = sparse_add(test_vec1, test_vec2)
+        self.assertEqual(actual, expected)
+
+    def test_sparse_add_different_length(self):
         expected = None
         test_vec1 = {'length': 7, 6: 3}
         test_vec2 = {'length': 5, 2: 7}
+        actual = sparse_add(test_vec1, test_vec2)
+        self.assertEqual(actual, expected)
+
+    def test_sparse_add_mixed_negative_values(self):
+        expected = {'length': 5, 1: -2, 3: 3, 4: -6}
+        test_vec1 = {'length': 5, 1: -2, 4: 1}
+        test_vec2 = {'length': 5, 3: 3, 4: -7}
+        actual = sparse_add(test_vec1, test_vec2)
+        self.assertEqual(actual, expected)
+
+    def test_sparse_add_mixed_key_orders(self):
+        expected = {'length': 7, 1: 4, 2: 4, 3: 2, 4: 2, 5: 1, 6: -2}
+        test_vec1 = {6: -2, 2: 3, 'length': 7, 3: 1, 1: 2}
+        test_vec2 = {5: 1, 1: 2, 2: 1, 4: 2, 3: 1, 'length': 7}
         actual = sparse_add(test_vec1, test_vec2)
         self.assertEqual(actual, expected)
