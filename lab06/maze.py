133a6a010e04d928a67d2b96a67412c8f4444516
Young Kim <young_github@chillin.dev>
2020-03-05 13:47:44 -0800

    completed move_choice() and move_character()

--- lab06/maze.py
+++ lab06/maze.py
@@ -26,6 +26,29 @@ def make_character() -> list:
     return [0, 0]
 
 
+def move_choice() -> str:
+    """
+    """
+    valid = False
+    while not valid:
+        move_choice = input("Where do you want to go? (up, down, left, "
+                            "or right): ").replace(" ", "").lower()
+        if move_choice in ["up", "down", "left", "right"]:
+            valid = True
+            break
+
+    return move_choice
+
+
+def move_character(character: dict, move_choice: str):
+    """
+    """
+    directions = {'right': [0, 1], 'left': [0, -1],
+                  'up': [1, -1], 'down': [1, 1]}
+
+    character[directions[move_choice][0]] += directions[move_choice][1]
+
+
 def main():
     """
     """
