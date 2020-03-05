f95af354c70e6f168ef450d9fd6d38a9adfa2004
Young Kim <young_github@chillin.dev>
2020-03-05 13:48:47 -0800

    completed validate_move() and check_reached_goal()

--- lab06/maze.py
+++ lab06/maze.py
@@ -6,6 +6,25 @@ Young Kim (A01087377)
 import doctest
 
 
+def game():
+    """
+    """
+    board = make_board()
+    character = make_character()
+    reached_goal = False
+    while not reached_goal:
+        print(f"You are now on {character}.")
+        user_move = move_choice()
+        if validate_move(board, character, user_move):
+            move_character(character, user_move)
+        else:
+            print("# ERROR: invalid input.")
+
+        if check_reached_goal(board, character):
+            print("\n=== Game End === You Win!! ===")
+            reached_goal = True
+
+
 def make_board() -> list:
     """
     """
@@ -49,10 +68,31 @@ def move_character(character: dict, move_choice: str):
     character[directions[move_choice][0]] += directions[move_choice][1]
 
 
+def validate_move(board: list, character: list, move_choice: str) -> bool:
+    """
+    """
+    board_size = [len(board[0]), len(board)]
+    directions = {'left': [0, -1], 'right': [0, 1],
+                  'up': [1, -1], 'down': [1, 1]}
+    xy = directions[move_choice][0]
+    move = directions[move_choice][1]
+    if move_choice in directions:
+        if 0 <= character[xy] + move < board_size[xy]:
+            return True
+    return False
+
+
+def check_reached_goal(board: list, character: list) -> bool:
+    """
+    """
+    goal_pos = [len(board[0]) - 1, len(board) - 1]
+    return True if character == goal_pos
+
+
 def main():
     """
     """
-    pass
+    game()
 
 
 if __name__ == "__main__":
