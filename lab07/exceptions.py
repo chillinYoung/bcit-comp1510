564d6500bb606c74865de993d832358343d9f9a9
Young Kim <young_github@chillin.dev>
2020-03-23 01:39:31 -0700

    added find_an_even() function

--- lab07/exceptions.py
+++ lab07/exceptions.py
@@ -33,6 +33,29 @@ def heron(num: int) -> float:
         return -1
 
 
+def find_an_even(input_list: list) -> int:
+    """Return the first even number in input_list.
+
+    :param input_list: a list of integers
+    :precondition: input_list must be a list of integer
+    :postcondition: return the first even number in input_list
+    :raise ValueError: if input_list does not contain an even number
+    :return: first even number in input_list
+
+    >>> test_list = [3, 1, 4, 6, 2]
+    >>> find_an_even(test_list)
+    2
+    >>> test_list = [3, 1, 5, 9, 10, 7, 11]
+    >>> find_an_even(test_list)
+    10
+    """
+    even_list = [num for num in input_list if num % 2 == 0]
+    try:
+        return min(even_list)
+    except ValueError as ve:
+        print(f"# Not Found Error: no even numbers in the list")
+
+
 def main():
     """Drive the doctest in this module."""
     doctest.testmod()
