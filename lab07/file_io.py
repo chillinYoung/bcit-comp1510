ecfd0f1b5930abedc569475b21737c950b858f60
Young Kim <young_github@chillin.dev>
2020-03-23 19:07:07 -0700

    added docstrings

--- lab07/file_io.py
+++ lab07/file_io.py
@@ -8,8 +8,11 @@ import re
 
 
 def clean_text(string_contents: str) -> list:
-    """
+    """Clean the text to have only alphabets.
 
+    :param string_contents: the contents as string
+    :precondition: string_contents must be given as string
+    :postconsition: correctly clean the text to have only alphabets
     :return: the list with cleaned and splited words
     """
     filtered = re.sub("[^ a-z\n]+", "", string_contents)
@@ -17,7 +20,14 @@ def clean_text(string_contents: str) -> list:
 
 
 def count_words_in_list(words_list: list) -> dict:
-    """
+    """Create a dictionary with counted words in the given list.
+
+    :param words_list: the list with words
+    :precondition: the words_list must be cleaned with clean_text function
+            in this module
+    :postcondition: correctly create a new dictionary contains counted words
+            information
+    :return: the dictionary contains counted words
     """
     word_counts = dict()
     for word in words_list:
@@ -27,7 +37,12 @@ def count_words_in_list(words_list: list) -> dict:
 
 
 def print_most_top_ten(word_counts: dict):
-    """
+    """Print the most top ten words.
+
+    :param word_counts: the dictionary with counted words information
+    :precondition: the wood_counts dict must be correctly formed with
+            count_words_in_list function in this module
+    :postcondition: correctly print the most top ten words
     """
     count_list = [(value, key) for key, value in word_counts.items()]
     count_list.sort(reverse=True)
@@ -37,12 +52,14 @@ def print_most_top_ten(word_counts: dict):
 
 
 def main():
-    """Drive the program and the doctest in this module."""
+    """Drive the program and the doctest in this module.
+
+    :raise FileNotFoundError: if the file name input by the user is not founded
+    """
     doctest.testmod()
 
-    # file_choose = input("Please enter the name of a file to open: ")
-    file_choose = "moby_dick.txt"
-
+    # run the program
+    file_choose = input("Please enter the name of a file to open: ")
     try:
         with open(file_choose) as text_file:
             contents = text_file.read().lower()
