921476af7668087a937e4eca459eccf663880a94
Young Kim <young_github@chillin.dev>
2020-03-23 17:15:37 -0700

    the prototype decomposed to the functions

--- lab07/file_io.py
+++ lab07/file_io.py
@@ -7,6 +7,35 @@ import doctest
 import re
 
 
+def clean_text(string_contents: str) -> list:
+    """
+
+    :return: the list with cleaned and splited words
+    """
+    filtered = re.sub("[^ a-z\n]+", "", string_contents)
+    return filtered.split()
+
+
+def count_words_in_list(words_list: list) -> dict:
+    """
+    """
+    word_counts = dict()
+    for word in words_list:
+        word_counts[word] = word_counts.get(word, 0) + 1
+
+    return word_counts
+
+
+def print_most_top_ten(word_counts: dict):
+    """
+    """
+    count_list = [(value, key) for key, value in word_counts.items()]
+    count_list.sort(reverse=True)
+
+    for item in count_list[0:10]:
+        print(f"{item[1]} - {item[0]}")
+
+
 def main():
     """Drive the program and the doctest in this module."""
     doctest.testmod()
@@ -20,17 +49,9 @@ def main():
     except FileNotFoundError as fnfe:
         print(f"# {fnfe}")
     else:
-        filtered = re.sub("[^ a-z\n]+", "", contents)
-        words = filtered.split()
-
-        word_counts = dict()
-        for word in words:
-            word_counts[word] = word_counts.get(word, 0) + 1
-
-        count_list = [(value, key) for key, value in word_counts.items()]
-        count_list.sort(reverse=True)
-        for item in count_list[0:10]:
-            print(f"{item[1]} - {item[0]}")
+        words = clean_text(contents)
+        word_counts = count_words_in_list(words)
+        print_most_top_ten(word_counts)
 
 
 if __name__ == "__main__":
